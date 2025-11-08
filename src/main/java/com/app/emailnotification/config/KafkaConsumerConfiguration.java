7f6f03d0e0af5f75dedba76f1eadf6a07d344973
Uday Teja <dev@udayteja.com>
2025-11-08 20:55:04 +0530

    update code

---
+++ src/main/java/com/app/emailnotification/config/KafkaConsumerConfiguration.java
@@ -0,0 +1,34 @@
+package com.app.emailnotification.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfiguration {
+
+    @Autowired
+    Environment environment;
+
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory() {
+        Map<String, Object> consumerConfigMap = new HashMap<>();
+
+        consumerConfigMap.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, environment.getProperty("spring.kafka.consumer.bootstrap-servers"));
+        consumerConfigMap.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        consumerConfigMap.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        consumerConfigMap.put(JsonDeserializer.TRUSTED_PACKAGES, environment.getProperty("spring.kafka.consumer.properties.spring.json.trusted.packages"));
+        consumerConfigMap.put(ConsumerConfig.GROUP_ID_CONFIG, environment.getProperty("spring.kafka.consumer.group-id"));
+
+        return new DefaultKafkaConsumerFactory<>(consumerConfigMap);
+    }
+}
---
+++ src/main/java/com/app/emailnotification/handler/ProductCreatedEventHandler.java
@@ -0,0 +1,20 @@
+package com.app.emailnotification.handler;
+
+import com.app.core.ProductCreateEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@KafkaListener(topics = {"product-created-events-topic"})
+public class ProductCreatedEventHandler {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    @KafkaHandler
+    public void handle(ProductCreateEvent productCreatedEvent) {
+        LOGGER.info("Received new event: " + productCreatedEvent.getTitle());
+    }
+}
